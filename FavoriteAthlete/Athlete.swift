02d9621b55eb1f0d480325c678e5d9317b33f95e
danismgomes <danismgomes@gmail.com>
2019-08-28 11:38:04 -0700

    first commit

---
+++ FavoriteAthlete/Athlete.swift
@@ -0,0 +1,9 @@
+//
+//  Athlete.swift
+//  FavoriteAthlete
+//
+//  Created by Danielle Gomes on 2019-08-28.
+//  Copyright © 2019 Danielle Gomes. All rights reserved.
+//
+
+import Foundation
---
+++ FavoriteAthlete/AthleteFormViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FavoriteAthlete
+//
+//  Created by Danielle Gomes on 2019-08-28.
+//  Copyright © 2019 Danielle Gomes. All rights reserved.
+//
+
+import UIKit
+
+class AthleteFormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
